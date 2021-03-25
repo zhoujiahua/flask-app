c8d81a0b9c7367e7c14af7f8dcf99aa681b808f6
jerryZhou <jerry.zhou@ptn.advantest.com>
2021-03-25 19:05:46 +0800

    Udpate pipfile

--- main.py
+++ main.py
@@ -5,30 +5,21 @@ from flask import Flask
 import requests
 import json
 from app.utils.read_file import encode_base64, decode_base64, read_img, printosinfo
-import os
-import sys
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home_page():
-    return 'Hi Python'
+    return 'Automatic script hook'
 
 
-@app.route('/layui')
-def layui_api():
-    url = 'https://www.layui.com/demo/table/user/?page=1&limit=30'
-    r = requests.get(url)
-    print(type(r.json()))
-    return r.json()
+@app.route('/hook/test')
+def hook_test():
+    with open('./static/to.json','rb') as f:
+        to_data = f.read()
+        return to_data
 
 
 if __name__ == '__main__':
-    # en_img = encode_base64('./images/icic.png')
-    # decode_base64(en_img)
-    # txt_data = read_img('./static/t.txt')
-    # decode_base64(txt_data)
-    # print(txt_data)
-    # printosinfo()
     app.run(debug=True)
