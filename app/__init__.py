ff4c5347904d2e1ba1c2a069c68a03f9e4cc3469
jerryZhou <jerry.zhou@ptn.advantest.com>
2021-03-26 18:53:06 +0800

    Add route params

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@
 # -*- coding: UTF-8 -*-
 
 from flask import Flask
+from flask_cors import CORS
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app,supports_credentials=True)
     # app.config.from_object('config')
     register_blueprint(app)
     return app
--- app/api/comm.py
+++ app/api/comm.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-
+from flask import jsonify
 from . import api_comm
 
 @api_comm.route("/api/comm/email")
 def api_comm_email():
     return 'send email api'
+
+@api_comm.route("/api/comm/search/<key>/<content>")
+def api_comm_search(key,content):
+    search_data = {'key':key, 'content':content}
+    return jsonify(search_data)
