f92fdbd7aeb2dcf2db2b8da1c2260d1e53b9a9bc
jerryZhou <jerry.zhou@ptn.advantest.com>
2021-03-29 17:44:04 +0800

    Update doc file

--- app/api/user.py
+++ app/api/user.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
+from app.models.user import User
 from . import api_user
+from sqlalchemy.orm import session
+
 
 @api_user.route("/api/user/login")
 def api_user_login():
+    # add_user = User()
+    # add_user.name = 'jerry'
+    # add_user.password = '123456'
+    # add_user.email = 'jerry.zhou@advantest.com'
+    # session.add(add_user)
+    # session.commit()
     return 'user login api'
+
 
 @api_user.route("/api/user/register")
 def api_user_register():
--- app/secure.py
+++ app/secure.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
-SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://topy:123456@10.150.253.30:3306/topy'
+config = {
+    "MYSQL": "mysql",
+    "CYMSQL": "cymysql",
+    "ACCOUNT": "topy",
+    "PASSWORD": 123456,
+    "ADDRESS": "10.150.253.30",
+    "PORT": 3306,
+    "DATABASENAME": "topy"
+}
+
+# SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://topy:123456@10.150.253.30:3306/topy'
+SQLALCHEMY_DATABASE_URI = '{MYSQL}+{CYMSQL}://{ACCOUNT}:{PASSWORD}@{ADDRESS}:{PORT}/{DATABASENAME}'.format(**config)
+
+# 为True时，flask-sqlalchemy会跟踪对象的修改
 SQLALCHEMY_TRACK_MODIFICATIONS = True
